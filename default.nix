4eb820fb362c554baa73e244dae29aa62ec2d9d5
William Casarin <jb55@jb55.com>
2020-11-09 13:10:30 -0800

    nix: build fixes

---
+++ default.nix
@@ -0,0 +1,20 @@
+{ stdenv, gmp, bison, flex }:
+
+stdenv.mkDerivation rec {
+  name = "ratio-${version}";
+  version = "0.1";
+
+  src = ./.;
+
+  makeFlags = "PREFIX=$(out)";
+
+  nativeBuildInputs = [ bison flex ];
+  buildInputs = [ gmp ];
+
+  meta = with stdenv.lib; {
+    description = "ratio calculator";
+    homepage = "https://github.com/jb55/ratio";
+    maintainers = with maintainers; [ jb55 ];
+    license = licenses.mit;
+  };
+}
